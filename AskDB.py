57fd88e7c8a401996d93d038549c4ac79b77402e
Will Hopper <whopper@cat.pdx.edu>
2012-01-21 17:41:36 -0800

    Added working MySQL query fuction for the noobz VLAN.
    Signed-off-by: Will Hopper <whopper@cat.pdx.edu>

--- AskDB.py
+++ AskDB.py
@@ -1,9 +1,31 @@
 #!/usr/bin/python
 
-import sys, socket, string, re, subprocess, _mysql
+import sys, socket, string, re, subprocess, _mysql 
+import MySQLdb
 
 def main ():
-  print "TEST"
+#  subprocess.call(['echo "host test: hostname- test, IP- test, purpose- test OS - test" > queryresult'], shell=True)
+
+  db=MySQLdb.Connection(host="localhost",user="whopper",
+                        passwd="l33tk1tt3h",db="combs_vlan_noobz")
+
+  cursor = db.cursor()
+
+  linefile = open("/home/whopper/CombsBot/queryresult")
+  toquery = []
+  linetemp = []
+  linetemp = linefile.readlines() 
+  for lines in linetemp:  
+    toquery.append(lines.rstrip('\r\n')) 
+
+  cursor.execute("SELECT * FROM box WHERE ip_address = '%s'" % toquery[0])
+
+
+  data = cursor.fetchall() 
+  for each in data:
+    print each
+
+  subprocess.call(['echo "%s" > queryresult' % data], shell=True)
 
 if __name__ == '__main__':
   main()
--- xCombsBot.py
+++ xCombsBot.py
@@ -2,81 +2,83 @@
 
 import sys, socket, string, re, random, subprocess, ssl
 
-HOST="iss.cat.pdx.edu"
-PORT=6697
-NICK="CombsBot"
-IDENT="CombsBot"
-REALNAME="Whopper's Bot"
-CHAN="#whopper"
-readbuffer=""
+def main():
+  HOST="iss.cat.pdx.edu"
+  PORT=6697
+  NICK="CombsBot"
+  IDENT="CombsBot"
+  REALNAME="Whopper's Bot"
+  CHAN="#catacombs"
+  readbuffer=""
 
-#Random Variables _________________________________________
-SerErrorCheck = 0
-s = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
-s = ssl.wrap_socket(s,
-                  cert_reqs=ssl.CERT_NONE,
-                  do_handshake_on_connect=True)
+  #Random Variables _________________________________________
+  SerErrorCheck = 0
+  s = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
+  s = ssl.wrap_socket(s,
+                    cert_reqs=ssl.CERT_NONE,
+                    do_handshake_on_connect=True)
 
-s.connect((HOST, PORT))
-s.send("NICK %s\r\n" % NICK)
-s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+  s.connect((HOST, PORT))
+  s.send("NICK %s\r\n" % NICK)
+  s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
 
-s.send("NICK %s\r\n" % NICK)
-s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
-s.send("JOIN %s :%s\r\n" % (CHAN, "#######"))
-# _________________________________________________________
+  s.send("NICK %s\r\n" % NICK)
+  s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+  s.send("JOIN %s :%s\r\n" % (CHAN, "#######"))
+  # _________________________________________________________
 
-while 1:
-  readbuffer=readbuffer+s.recv(4096)
-  temp=string.split(readbuffer, "\n")
-  readbuffer=temp.pop( )
+  while 1:
+    readbuffer=readbuffer+s.recv(4096)
+    temp=string.split(readbuffer, "\n")
+    readbuffer=temp.pop( )
 
-  for line in temp:
-    line=string.rstrip(line)
-    line=string.split(line)
-    print line
+    for line in temp:
+      line=string.rstrip(line)
+      line=string.split(line)
+      print line
 
-# Lengthtemp is used throughout the script, mainly to check to syntax errors in input
-    lengthtemp = len(line)
-    if lengthtemp >= 4: # only parse lines if there are at least 3 words
-# _______________________________OPTIONS ________________________________________
-      if line[3] == ':!help':
-        s.send("PRIVMSG %s :%s\r\n" % (CHAN, "Options: !host $host/IP - gives info about a Combs box"))
+  # Lengthtemp is used throughout the script, mainly to check to syntax errors in input
+      lengthtemp = len(line)
+      if lengthtemp >= 4: # only parse lines if there are at least 3 words
+  # _______________________________OPTIONS ________________________________________
+        if line[3] == ':!help':
+          s.send("PRIVMSG %s :%s\r\n" % (CHAN, "Options: !host $host/IP - gives info about a Combs box"))
 
-      if line[3] == ':!host' or '!host' in line:
-        subprocess.call(["/home/whopper/CombsBot/AskDB.py"], shell=True)
-        FileWrite(s, CHAN)
-
-
-
-
+        if line[3] == ':!host' or '!host' in line:
+          subprocess.call(['echo %s > queryresult' % line[4]], shell=True)
+          subprocess.call(["/home/whopper/CombsBot/AskDB.py"], shell=True)
+          FileWriter(s, CHAN)
 
 
-# Keeps bot alive 
-    if(line[0]=="PING"):
-      s.send("PONG %s\r\n" % line[1])
 
-def FileWriter(s, CHAN):.
- tempfile = open("/home/whopper/CombsBot/queryresult")
- i = 0
- k = 0
- tlist1 = []
- tlist2 = []
 
- tlist2 = tempfile.readlines()
- tlist2length = len(tlist2)
- for each in tlist2:
-   tlist1.append(each.rstrip('\r\n'))
-   i = i + 1
- if i > 1:
-   for each in tlist2:
-     while k < tlist2length:
-       s.send("PRIVMSG %s :%s\r\n" % (CHAN, "%s" % tlist2[k]))
-       k = k + 1
- else:
-   s.send("PRIVMSG %s :%s\r\n" % (CHAN, "%s" % tlist1[0]))
 
- subprocess.call(['rm /home/whopper/CombsBot/queryresult'], shell=True)
+  # Keeps bot alive 
+      if(line[0]=="PING"):
+        s.send("PONG %s\r\n" % line[1])
+
+def FileWriter(s, CHAN): 
+ 
+  tempfile = open("/home/whopper/CombsBot/queryresult")
+  i = 0
+  k = 0
+  tlist1 = []
+  tlist2 = []
+
+  tlist2 = tempfile.readlines()
+  tlist2length = len(tlist2)
+  for each in tlist2:
+    tlist1.append(each.rstrip('\r\n'))
+    i = i + 1
+  if i > 1:
+    for each in tlist2:
+      while k < tlist2length:
+        s.send("PRIVMSG %s :%s\r\n" % (CHAN, "%s" % tlist2[k]))
+        k = k + 1
+  else:
+    s.send("PRIVMSG %s :%s\r\n" % (CHAN, "%s" % tlist1[0]))
+
+#  subprocess.call(['rm /home/whopper/CombsBot/queryresult'], shell=True)
 
 
 if __name__ == '__main__':
