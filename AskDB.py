2a74f3f5fe48247822def9c3a47f2664b0d48b31
Will Hopper <whopper@cat.pdx.edu>
2012-01-21 19:24:04 -0800

    Fixed some formatting. Noobz VLAN query function complete.
    Signed-off-by: Will Hopper <whopper@cat.pdx.edu>

--- AskDB.py
+++ AskDB.py
@@ -7,9 +7,8 @@ def main ():
 #  subprocess.call(['echo "host test: hostname- test, IP- test, purpose- test OS - test" > queryresult'], shell=True)
 
   db=MySQLdb.Connection(host="localhost",user="whopper",
-                        passwd="l33tk1tt3h",db="combs_vlan_noobz")
+                        passwd="#######",db="combs_vlan_noobz")
 
-  cursor = db.cursor()
 
   linefile = open("/home/whopper/CombsBot/queryresult")
   toquery = []
@@ -18,14 +17,20 @@ def main ():
   for lines in linetemp:  
     toquery.append(lines.rstrip('\r\n')) 
 
+  cursor = db.cursor()
   cursor.execute("SELECT * FROM box WHERE ip_address = '%s'" % toquery[0])
 
-
   data = cursor.fetchall() 
-  for each in data:
-    print each
+  strdata = str(data)
+  print strdata[22:25]
+  print ":ALALALA"
+  if strdata[22:25] == '\\n\'':
+    subprocess.call(['echo "No host registered for IP %s" > queryresult' % toquery], shell=True)
 
-  subprocess.call(['echo "%s" > queryresult' % data], shell=True)
+  else:
+    subprocess.call(['echo "Results: Format: IP - Username - Hostname - OS - Purpose - Hardware" > queryresult'], shell=True)
+    subprocess.call(['echo "%s" >> queryresult' % data], shell=True)
+
 
 if __name__ == '__main__':
   main()
--- xCombsBot.py
+++ xCombsBot.py
@@ -7,11 +7,11 @@ def main():
   PORT=6697
   NICK="CombsBot"
   IDENT="CombsBot"
-  REALNAME="Whopper's Bot"
+  REALNAME="Whopper's Bot. Uses Wicker's CombsDB as backend"
   CHAN="#catacombs"
   readbuffer=""
 
-  #Random Variables _________________________________________
+ #Random Variables _________________________________________
   SerErrorCheck = 0
   s = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
   s = ssl.wrap_socket(s,
@@ -27,6 +27,7 @@ def main():
   s.send("JOIN %s :%s\r\n" % (CHAN, "#######"))
   # _________________________________________________________
 
+
   while 1:
     readbuffer=readbuffer+s.recv(4096)
     temp=string.split(readbuffer, "\n")
@@ -41,10 +42,10 @@ def main():
       lengthtemp = len(line)
       if lengthtemp >= 4: # only parse lines if there are at least 3 words
   # _______________________________OPTIONS ________________________________________
-        if line[3] == ':!help':
+        if line[3] == ':&help':
           s.send("PRIVMSG %s :%s\r\n" % (CHAN, "Options: !host $host/IP - gives info about a Combs box"))
 
-        if line[3] == ':!host' or '!host' in line:
+        if line[3] == ':&host' or '&host' in line:
           subprocess.call(['echo %s > queryresult' % line[4]], shell=True)
           subprocess.call(["/home/whopper/CombsBot/AskDB.py"], shell=True)
           FileWriter(s, CHAN)
